6428cfc47f36d263f46d3c2fd6f4f3c810d6ecc6
Suraj Verma <stdmail2552@gmail.com>
2023-06-24 16:45:46 +0530

    Updated input images

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python 3.9" # change py version as per your need
-conda create --prefix ./env python=3.9 -y
+conda create --prefix ./env39 python=3.9 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "installing the requirements" 
